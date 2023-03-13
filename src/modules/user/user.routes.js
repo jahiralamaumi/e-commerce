322fffc608e19c5158a9b0285fda1f00328f6846
jahiralamaumi <jahiralam25@gmail.com>
2023-03-14 03:44:47 +0600

    password encryption, login api, token generate, authenticate middleware, update data using token authentication

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,14 +1,17 @@
 const path = require('path');
-const { createUser, getUsers, updateUser, deleteUser } = require('./user.controller');
-const { createUserSchema } = require('./user.schema');
+const { createUser, login, getUsers, updateUser, deleteUser } = require('./user.controller');
+const { createUserSchema, updateUserSchema } = require('./user.schema');
 const { validate } = require(path.join(process.cwd(), '/src/modules/core/middlewares/validate'));
+const authenticate = require(path.join(process.cwd(), '/src/modules/core/middlewares/authenticate'));
 
 module.exports = function userRoutes(app){
     app.route('/users')
     .get(getUsers)
     .post(validate(createUserSchema), createUser);
 
+    app.post('/users/login', login);
+
     app.route('/users/:email')
-    .patch(updateUser)
+    .patch(authenticate, validate(updateUserSchema), updateUser)
     .delete(deleteUser);
 }
